b26bcfef11d17b3914ad2ed44abef91fba9e3cba
Karthika Shanmugam <karthikashanmugam2005@gmail.com>
2026-02-11 11:24:05 +0530

    Time: 14 ms (92.89%) | Memory: 51.5 MB (54.51%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n=g.length;
+        int m=s.length;
+        int i =0;
+        int j=0;
+        while(i<m && j<n){
+            if(g[j]<=s[i])j++;
+            i++;
+        }
+        return j;
+       
+    }
+}
